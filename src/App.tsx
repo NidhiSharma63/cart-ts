f66912fc4bdb99cbfc75e170e2ed5c133bff3126
nidhi.sharma@dronebase.com <nidhi.sharma@dronebase.com>
2023-03-05 20:03:05 +0530

    navbar functionality added

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,21 @@
-import React from 'react';
-import {Navigate, useRoutes} from 'react-router-dom';
-import Home from './pages/Home';
-import Cart from './pages/Cart';
-import Navbar from './components/Navbar';
+import React from "react";
+import { Navigate, useRoutes } from "react-router-dom";
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+import Navbar from "./components/Navbar";
 
 const App: React.FC = (): JSX.Element => {
-
   const Routes = {
-    path: '/',
+    path: "/",
     element: <Navbar />,
     children: [
-      {path: '/', element: <Home />},
-      {path:'cart',element:<Cart/>}
+      { path: "/home", element: <Home /> },
+      { path: "cart", element: <Cart /> },
     ],
   };
   const routing = useRoutes([Routes]);
 
   return <>{routing}</>;
-  
-}
+};
 
 export default App;
